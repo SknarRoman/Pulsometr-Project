22d0c858fece940eb7eabac0ca2800c21fce488b
Roman Sknar <Sknarroma@gmail.com>
2022-08-29 20:57:29 +0300

    fix the crime

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -18,7 +18,7 @@ $mail->Password = 'ezqqgwhzeyiivkjf';                           // Наш пар
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('sknarroma@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('', 'Pulse');   // От кого письмо 
 $mail->addAddress('henol98435@vasqa.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
